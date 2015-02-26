44a43acead1da83b82678afc3df5cb43d202d966
Patricio Ramirez <pramirez@csrg.inf.utfsm.cl>
2015-02-26 16:47:10 -0300

    Documentatin, ConfigParser, Bug fix, Code arrenge, Insert_bulk

--- ACM/acmmodules/ACMconfig.py
+++ ACM/acmmodules/ACMconfig.py
@@ -1,15 +1,17 @@
 import ConfigParser, os
 
-class acmConfigParser(object):
+class acmConfigParser():
 	config_data = {'xbee':{'port':'/dev/ttyO1',
                            'baud':9600,
                            'uart':None},
-                   'sqlite':{'dbDir':'database/acm.bd',
-                   	         'xbeeCSV':'files/xbee_data.csv'},
-                   'mqtt':{'destIp':'127.0.0.1',
-                           'destPort':1883,
-                           'topicWill':'ACM/BBB/status',
-                           'lastWill':'ERROR: BBB Lost'}}
+                   'sqlite':{'db_dir':'database/acm.bd',
+                   	         'xbee_csv':'files/xbee_data.csv'},
+                   'mqtt':{'dest_ip':'localhost',
+                           'dest_port':1883,
+                           'topic_will':'ACM/BBB/status',
+                           'last_will':'ERROR: BBB Lost'},
+                   'general':{'month_gap':3,
+                              'sleep_time':150}}
 	def __init__(self):
 		self.config = ConfigParser.RawConfigParser()
 		if not(os.path.isfile('acm.cfg')):
@@ -22,3 +24,19 @@ class acmConfigParser(object):
 		else:
 			self.config.read('acm.cfg')
 
+	def getConf(self,section,*args):
+		data = {}
+		if self.config.has_section(section):
+			for row in self.config_data[section]:
+				if row not in args:
+					if self.config.has_option(section,row):
+						data[row] = self.config.get(section,row)
+					else:
+						raise ValueError('No configuration for %s, %s', (row,section))
+		return data
+
+	def getGeneralConf(self,general_conf):
+		if self.config.has_option('general',general_conf):
+			return self.config.get('general',general_conf)
+		else:
+			raise ValueError('No configuration for %s,%s', (general_conf,'general')) 
